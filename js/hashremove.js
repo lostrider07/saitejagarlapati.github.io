e48a22f5749ef3551f77a6e933fa0e738957178e
lostrider07 <4511saiteja@gmail.com>
2022-11-27 23:05:55 -0800

    adding js files

---
+++ js/hashremove.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  const menuBtn = $(".hashremove");
+  menuBtn.click(() => {
+    setTimeout(() => {
+      removeHash();
+    }, 1);
+  });
+  function removeHash() {
+    history.replaceState(
+      "",
+      document.title,
+      window.location.origin + window.location.pathname + window.location.search
+    );
+  }
+});
---
+++ js/load.js
@@ -0,0 +1,11 @@
+var projs = [];
+var links = [
+  "https://github.com/lostrider07/Crop_prediction_using_ML",
+  "https://github.com/lostrider07/Accuracy-of-enterprise-data-using-ML",
+  "https://github.com/lostrider07/Front-end-project--Bigboard",
+  "https://github.com/lostrider07/SocialMediaApplication",
+];
+for (i = 0; i < links.length; i++) {
+  projs.push(new Image());
+  projs[i].src = links[i];
+}
