0a443cb1ab512ab773f6549b19bdafe0d1570041
lostrider07 <4511saiteja@gmail.com>
2022-11-28 16:11:10 -0800

    replacing jQuery with $ in defining a function in slidechange

--- js/slidechange.js
+++ js/slidechange.js
@@ -1,5 +1,5 @@
 var changeSlide = 0;
-jQuery(".portnxt").click(function ($) {
+$(".portnxt").click(function () {
   $(".your-class").slick("slickNext");
   changeSlide = (changeSlide + 1) % 7;
   if (changeSlide == 0) {
@@ -100,11 +100,11 @@ jQuery(".portnxt").click(function ($) {
     $(".port2").addClass("outerFourth");
   }
   $(".portnxt").attr("disabled", "disabled");
-  setTimeout(function () {
+  setTimeout(function ($) {
     $(".portnxt").removeAttr("disabled");
   }, 500);
 });
-$(".portprev").click(function ($) {
+$(".portprev").click(function () {
   $(".your-class").slick("slickPrev");
   changeSlide = changeSlide - 1;
   changeSlide = (changeSlide + 7) % 7;
@@ -207,7 +207,7 @@ $(".portprev").click(function ($) {
     $(".port0").addClass("outerFirst");
   }
   $(".portprev").attr("disabled", "disabled");
-  setTimeout(function ($) {
+  setTimeout(function () {
     $(".portprev").removeAttr("disabled");
   }, 500);
 });
